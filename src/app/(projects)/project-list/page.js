ad4163f918a3fcc2643e31253789dd214404dc73
arafay493 <arafay493@gmail.com>
2024-12-10 16:46:06 +0500

    Appling TanStack Table for the Project List.

--- src/app/(projects)/project-list/page.js
+++ src/app/(projects)/project-list/page.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import ProjectListTable from "@/components/projectsComponents/project-list/ProjectListTable";
+import { Card, CardContent, Typography } from "@mui/material";
+import React from "react";
 
 const ProjectList = () => {
   return (
-    <div>ProjectList</div>
-  )
-}
+    <Card>
+      <CardContent>
+        <Typography variant="h3" fontSize={20} mb={2} fontWeight={600}>
+          Project List
+        </Typography>
+        <ProjectListTable />
+      </CardContent>
+    </Card>
+  );
+};
 
-export default ProjectList
+export default ProjectList;
---
+++ src/components/projectsComponents/project-list/ProjectListTable.js
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+const ProjectListTable = () => {
+  return <div>Project List Table</div>;
+};
+
+export default ProjectListTable;
