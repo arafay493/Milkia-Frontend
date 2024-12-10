9ee4a12e31b8b676fd18cd14ab90536d50723631
arafay493 <arafay493@gmail.com>
2024-12-10 14:19:08 +0500

    Input Designs Created in the AddProjectFrom.

--- src/app/(projects)/add-project/page.js
+++ src/app/(projects)/add-project/page.js
@@ -1,12 +1,11 @@
 import AddProjectForm from "@/components/projectsComponents/add-project/AddProjectForm";
 import { Card, CardContent, Typography } from "@mui/material";
 import React from "react";
-
 const AddProject = () => {
   return (
     <Card>
       <CardContent>
-        <Typography variant="h3" fontSize={20}>
+        <Typography variant="h3" fontSize={20} mb={2} fontWeight={600}>
           Add Project
         </Typography>
         <AddProjectForm />
--- src/components/projectsComponents/add-project/AddProjectForm.js
+++ src/components/projectsComponents/add-project/AddProjectForm.js
@@ -1,9 +1,89 @@
-import React from 'react'
+"use client";
+import { useFormik } from "formik";
+import React from "react";
+import styles from "../../../styles/add-project.module.css";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid2";
 
 const AddProjectForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      projectName: "",
+      location: "",
+      startPrice: "",
+    },
+    onSubmit: (values) => {
+      // alert(JSON.stringify(values, null, 2));
+      console.log(values);
+    },
+  });
   return (
-    <div>AddProjectForm</div>
-  )
-}
+    <Box sx={{ flexGrow: 1 }}>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid size={{ xs: 6 }}>
+            <label htmlFor="projectName" className={styles.label}>
+              Project Name
+            </label>
+            <br />
+            <input
+              className={styles.input}
+              id="projectName"
+              name="projectName"
+              type="text"
+              placeholder="Project name"
+              onChange={formik.handleChange}
+              value={formik.values.projectName}
+            />
+          </Grid>
+          <Grid size={{ xs: 6 }}>
+            <label htmlFor="location" className={styles.label}>
+              Multi City/Location/SubLocation
+            </label>
+            <br />
+            <input
+              className={styles.input}
+              id="location"
+              name="location"
+              type="text"
+              placeholder="Location"
+              onChange={formik.handleChange}
+              value={formik.values.location}
+            />
+          </Grid>
+          <Grid size={{ xs: 6 }}>
+            <label htmlFor="startPrice" className={styles.label}>
+              Start Price
+            </label>
+            <br />
+            <input
+              className={styles.input}
+              id="startPrice"
+              name="startPrice"
+              type="text"
+              placeholder="Start Price"
+              onChange={formik.handleChange}
+              value={formik.values.startPrice}
+            />
+          </Grid>
+          <Grid
+            size={{ xs: 12 }}
+            spacing={2}
+            textAlign={"center"}
+            display={"flex"}
+            gap={3}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <button type="submit" className={styles.button}>
+              Submit
+            </button>
+            <button className={styles.resetButton}>Reset</button>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
+  );
+};
 
-export default AddProjectForm
+export default AddProjectForm;
