279d3c6cb69ecbd8aa31b1d557618ad07e7050da
arafay493 <arafay493@gmail.com>
2024-12-13 11:13:51 +0500

    Heading Changed.

--- src/app/(Dealers)/registered-clients/page.js
+++ src/app/(Dealers)/registered-clients/page.js
@@ -8,7 +8,7 @@ const RegisteredClients = () => {
     <Card>
       <CardContent style={{ overflowX: "auto" }}>
         <Typography variant="h3" fontSize={20} mb={2} fontWeight={600}>
-          Project List
+          Registered Clients
         </Typography>
         <RegisteredClientsListTable />
       </CardContent>
