9f7ca29a07abfe6b8ec345b933b7d5edb1153649
arafay493 <arafay493@gmail.com>
2024-12-26 11:05:29 +0500

    Delete Modal Also Changed Created.

--- src/components/projectsComponents/project-list/DeleteModal.tsx
+++ src/components/projectsComponents/project-list/DeleteModal.tsx
@@ -1,5 +1,7 @@
 import StaticInputs from "@/components/StaticInputs/StaticInputs";
 import React from "react";
+import toast from "react-hot-toast";
+import { useDispatch } from "react-redux";
 import {
   Button,
   Col,
@@ -9,23 +11,55 @@ import {
   ModalHeader,
   Row,
 } from "reactstrap";
-const DeleteModal = ({ isOpen, toggle, row }) => {
+
+interface Details {
+  projectId: number;
+  projectName: string;
+  location: string;
+  city: string;
+  properties: number;
+}
+
+type DeleteModalProps = {
+  isOpen: boolean;
+  toggle: () => void;
+  row: Details | null;
+};
+
+const DeleteModal: React.FC<DeleteModalProps> = ({ isOpen, toggle, row }) => {
   const { projectId, projectName, location, city, properties } = row;
+  const dispatch = useDispatch();
+
+  const confirmDelete = async () => {
+    // if (row) {
+    // const res = await DeleteMasterItemType({ id: row.id });
+    // if (res.succeeded === false) {
+    //   toast.error(res.message);
+    //   toggle();
+    // } else {
+    //   toast.success(res.message);
+    //   // dispatch(fetchAllMasterItemTypes());
+    //   toggle();
+    // }
+    // }
+    toast.success("Successfully deleted")
+    toggle()
+  };
   return (
     <Modal isOpen={isOpen} toggle={toggle} size="lg" zIndex={1200}>
-      <ModalHeader toggle={toggle}>Delete Details</ModalHeader>
-      <ModalBody>
-        <StaticInputs
-          labels={[
-            "Project ID",
-            "Project Name",
-            "Location",
-            "City",
-            "Properties",
-          ]}
-          values={[projectId, projectName, location, city, properties]}
-        />
-      </ModalBody>
+      <ModalHeader toggle={toggle}>Confirm Deletion</ModalHeader>
+      {/* <ModalBody>
+        Are you sure you want to delete the item "{details?.name}"?
+      </ModalBody> */}
+      <ModalBody>Are you sure you want to delete this item?</ModalBody>
+      <ModalFooter>
+        {/* <Button color="primary" onClick={toggle}>
+          Cancel
+        </Button> */}
+        <Button color="danger" onClick={confirmDelete}>
+          Yes, Delete
+        </Button>
+      </ModalFooter>
     </Modal>
   );
 };
